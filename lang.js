15390cf0093cf2f7e0c7b30d974ca614df04910b
nikolabogdanov <nikola.k.bogdanov@gmail.com>
2017-06-27 18:09:38 +0300

    first stuff

---
+++ lang.js
@@ -0,0 +1,43 @@
+var currentLanguage = "bg";
+
+function lang() {
+    return {
+        "en":{
+            "mainTitle": "IT Law Services",
+            "contacts":"Contacts",
+            "about":"About",
+            "portfolio":"Portfolio",
+            "aboutFirst": "Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.",
+            "aboutSecond" : "Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!"
+        },
+        "bg":{
+            "mainTitle": "ИТ Правни Услуги",
+            "contacts":"Контакти",
+            "about":"За нас",
+            "portfolio":"Портфолио",
+            "aboutFirst": "Тралалалала 1",
+            "aboutSecond": "Тралала 2"
+        }
+    }
+}
+
+function getWord(string) {
+    var obj = lang();
+
+    if (currentLanguage === 'bg') {
+        return obj.bg[string]
+    } else {
+        return obj.en[string];
+    }
+}
+
+function translate() {
+    var arr = $('[nvalue]');
+    for (var i=0; i < arr.length; i++){
+        el = arr[i];
+        val = $(el).attr('nvalue');
+        el.innerHTML = getWord(val) + el.innerHTML;
+    }
+}
+
+
