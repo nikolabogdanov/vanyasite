a19b535fc4735da7211c3c476717eec206661048
Nikola <nikola.k.bogdanov@gmail.com>
2017-07-24 23:41:24 +0300

    request form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-function test() {
-	alert('test')
+function setControlls() {
+	$("#sendRequest").click(expandRequest);
+}
+
+function expandRequest(e) {
+	if($(this).height() !== 500){
+		$(this).animate({height:500},500);		
+	} else {
+		$(this).animate({height:70},500);	
+	}
+
+	e.preventDefault();
 }
